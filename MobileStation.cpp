09682068c286c077aa28f77117afd1131d9e0308
samgao001 <sam_001.gao@yahoo.com>
2014-05-16 11:50:38 -0400

    branch xusheng

--- MobileStation.cpp
+++ MobileStation.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/******************************* Constructor ***********************************************/
 MobileStation::MobileStation(){
 	stationNumber = rand() % 10 + 1;
 }
@@ -14,10 +15,12 @@ MobileStation::MobileStation(int number, BaseStation * station){
 	wait_time = 0.0;
 }
 
+/********************************** CSMA **************************************************/
 void MobileStation::backoff(int accessClass){
 	int backoff_time;
 	int cw_min = 0;
 	int cw_max = 0;
+
 	switch(accessClass){
 		case BEST_EFFORT:
 			backoff_time = rand() % (CWMAX_BE - CWMIN_BE) + CWMIN_BE;
@@ -35,6 +38,7 @@ void MobileStation::backoff(int accessClass){
 			backoff_time = rand() % (CWMAX_DA - CWMIN_DA) + CWMIN_DA;
 			break;
 	}
+
 	usleep(backoff_time * 1000);//use usleep?
 }
 
@@ -62,6 +66,7 @@ bool MobileStation::transmit(int accessClass){
 	
 	//Wait for open channel
 	waitStandard(accessClass);
+
 	//random wait for collisions?
 	int wait = rand() % 30000;
 	usleep(wait);
@@ -70,18 +75,23 @@ bool MobileStation::transmit(int accessClass){
 	cout << "Station " << stationNumber << " attempting to transmit" << endl;
 	bool attempt = baseStationName -> attemptTransmission(this);
 	std::clock_t start;
-    start = std::clock();
+    	start = std::clock();
+
 	while(!attempt){
 		cout << "Station " << stationNumber << " Backing off" << endl;
+
 		backoff(accessClass);//back off
 		waitStandard(accessClass);
+
 		//cout << "Station " << stationNumber << " re-attempting" << endl;
 		attempt = baseStationName -> attemptTransmission(this); //try again
 	}
+
 	delay_time += ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
 
 	//pretend to transmit
 	pretendTransmitting(accessClass);
+
 	//finish
 	cout << "Station " << stationNumber << " Tx Sucess" << endl;
 	baseStationName->finishTransmission();
@@ -91,21 +101,6 @@ bool MobileStation::transmit(int accessClass){
 
 void MobileStation::debug_printStatus(){
 	cout << "LOL" << endl;
-}
-
-int MobileStation::pickPacket(void){
-	//check if empty
-	if((packets[0] + packets[1] + packets[2] + packets[3] + packets[4]) == 0)
-		return -1;
-	int picked = -1;
-	while (picked == -1){
-		int check = rand() % 5;
-		if(packets[check] > 0){
-			picked = check;
-			packets[check]--;
-		}
-	}
-	return picked;
 }
 
 void MobileStation::pretendTransmitting(int accessClass){
@@ -132,7 +127,8 @@ void MobileStation::pretendTransmitting(int accessClass){
 void MobileStation::waitStandard(int accessClass){
 	bool status = baseStationName -> getChannelStatus();
 	std::clock_t start;
-    start = std::clock();
+	start = std::clock();
+
 	do {
 		//cout << "Station " << stationNumber << " waiting DIFS" << endl;
 		usleep(DIFS);//Wait DIFS
@@ -142,44 +138,66 @@ void MobileStation::waitStandard(int accessClass){
 		//cout << "Station " << stationNumber << " Sensing Channel" << endl;
 		status = baseStationName -> getChannelStatus();
 	} while(status==BUSY);
+
 	wait_time += ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
 }
+
+/******************************** Operations ************************************************/
 
 void MobileStation::main_loop(){
 	
 	std::clock_t start;
-    double duration;
-    start = std::clock();
-    
-    std::ofstream log;//writing
-    std::stringstream ss;//create a stringstream
+	double duration;
+	start = std::clock();
+
+	std::ofstream log;//writing
+	std::stringstream ss;//create a stringstream
 	ss << stationNumber;//add number to the stream
 	string log_name = std::string("Station_") + ss.str() + std::string(".txt");
 	//cout << "log name: " << log_name << endl;
 	log.open(log_name.c_str(), std::fstream::in | std::fstream::out | std::fstream::trunc); //problem
-    
-    //Keep Track of what gets tx
-    log << log_name << endl;
-    log << "Transmitting:" << endl;
-    log << "Best Effort\t-\t" << packets[0] << endl;
-    log << "Background\t-\t" << packets[1] << endl;
-    log << "Video\t\t-\t" << packets[2] << endl;
-    log << "Voice\t\t-\t" << packets[3] << endl;
-    log << "Data\t\t-\t" << packets[4] << endl;
-    
-    //Transmit loop
+
+	//Keep Track of what gets tx
+	log << log_name << endl;
+	log << "Transmitting:" << endl;
+	log << "Best Effort\t-\t" << packets[0] << endl;
+	log << "Background\t-\t" << packets[1] << endl;
+	log << "Video\t\t-\t" << packets[2] << endl;
+	log << "Voice\t\t-\t" << packets[3] << endl;
+	log << "Data\t\t-\t" << packets[4] << endl;
+
+	//Transmit loop
 	int newPacket = pickPacket();
 	while(newPacket != -1){
 		transmit(newPacket);
 		newPacket = pickPacket();
 	}
-	
+
 	std::cout << "Station " << stationNumber << " Transmissions completed" << endl;
-	
+
 	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
 	std::cout << "Total time: " << duration << " seconds" << endl;
 	log << "Total time: " << duration << " seconds" << endl;
 	log << "Wait time: " << wait_time << " seconds" << endl;
 	log << "Delay time: " << delay_time << " seconds" << endl;
 	log.close();
+}
+
+int MobileStation::pickPacket(void){
+	//check if empty
+	if((packets[0] + packets[1] + packets[2] + packets[3] + packets[4]) == 0){
+		return -1;
+	}
+
+	int picked = -1;
+
+	while (picked == -1){
+		int check = rand() % 5;
+		if(packets[check] > 0){
+			picked = check;
+			packets[check]--;
+		}
+	}
+
+	return picked;
 }
--- MobileStationQueue.cpp
+++ MobileStationQueue.cpp
@@ -25,23 +25,28 @@ int main( int argc, const char* argv[] ){
 	//MS setup
 	std::ifstream infile(argv[1]);
 	int stationNumber, be, bk, vi, vo, da;
+
 	while(infile >> stationNumber >> be >> bk >> vi >> vo >> da){
 		//cout << "CheckpointX" << endl;
 		MobileStation * temp = new MobileStation(stationNumber, base);
+
 		temp->packets[0] = be;
 		temp->packets[1] = bk;
 		temp->packets[2] = vi;
 		temp->packets[3] = vo;
 		temp->packets[4] = da;
+
 		base->addStation(*temp);
 	}
 	
 	//startup MS
 	//cout << "Checkpoint" << endl;
 	boost::thread_group mobileThreads;
+
 	for(std::vector<MobileStation>::iterator it = base->connectedStations.begin(); it != base->connectedStations.end(); ++it) {
 		mobileThreads.add_thread(new boost::thread(startMobileStation, *it));
 	}
+
 	mobileThreads.join_all();
 	
 	return 0;
